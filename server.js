72a7d313d49e3ca2744cb9e48dfcda69ebc8bec9
iFound <ifound2026@gmail.com>
2026-02-17 01:16:12 +0800

    Add root route to server.js

--- server.js
+++ server.js
@@ -18,17 +18,16 @@ mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log("MongoDB Connected"))
   .catch(err => console.log(err));
 
-// Root route
-app.get('/', (req, res) => {
-  res.send('iFound Backend is running!');
-});
-
 // Routes
 app.use('/api/auth', authRoutes);
 app.use('/api/admin', adminAuthRoutes);
 
+// Root route
+app.get('/', (req, res) => {
+  res.send('Welcome to iFound Backend!');
+});
+
 // Start Server
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+app.listen(5000, () => {
+  console.log("Server running on port 5000");
 });
